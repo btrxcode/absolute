ecba3e2b4b442ea4904841ed292e81b755545bc3
Beatrix <bealearnscode@gmail.com>
2019-04-09 14:41:59 +0000

    first commit

---
+++ absolute.cpp
@@ -0,0 +1,17 @@
+// Beatrix House
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <class T>
+T absoluteValue (T num)
+{
+    return abs(num);
+}
+
+int main()
+{
+    return 0;
+}
